8ca672dea79693c7edc4625bbf8643cd5077614d
theinfinity1410 <swayamgosavi1410@gmail.com>
2025-01-25 16:58:07 +0530

    Scrapped college ID field as students from other college might play as well

--- models/User.js
+++ models/User.js
@@ -7,11 +7,6 @@ const User = db.define('User', {
     allowNull: false,
     unique: true
   },
-  collegeId: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
   password: {
     type: DataTypes.STRING,
     allowNull: false,
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {login}  = require('../controllers/authController');
-const authMiddleware = require('../middlewares/authMiddlewares');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
